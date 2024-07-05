5a5e2577b9f45b8ca5c7906f24b9b8ccf57632a9
Kylo-the-noob <kylekmatthew15@gmail.com>
2024-07-05 13:40:54 -0400

    Added the operate function

--- calc.js
+++ calc.js
@@ -13,3 +13,16 @@ function divide(){
 function multiply(){
     return num1 * num2;
 }
+
+
+function operate(){
+    if(operator == "+"){
+        return add(num1, num2);
+    } else if (operator == "-"){
+        return subtract(num1, num2);
+    } else if (operator == "/"){
+        return divide(num1, num2);
+    } else if (operator == '*'){
+        return multiply(num1, num2);
+    }
+}
