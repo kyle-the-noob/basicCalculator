c82ce1a71f71fdffb9466c1883374fbd25467016
Kylo-the-noob <kylekmatthew15@gmail.com>
2024-07-05 13:40:00 -0400

    Added all operator functions

---
+++ calc.js
@@ -0,0 +1,15 @@
+function add(){
+    return num1 + num2
+}
+
+function subtract(){
+    return num1 - num2
+}
+
+function divide(){
+    return num1 / num2;
+}
+
+function multiply(){
+    return num1 * num2;
+}
