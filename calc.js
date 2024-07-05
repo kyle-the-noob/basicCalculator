f65d87f15daf3d6848467d73fb2245b13b32e519
Kylo-the-noob <kylekmatthew15@gmail.com>
2024-07-05 14:15:51 -0400

    Operate function now updates display.value

--- calc.js
+++ calc.js
@@ -45,29 +45,29 @@ const equalBtn = document.querySelector("#equal").onclick = () => {
 }
 
 function add(){
-    return num1 + num2
+    return +num1 + +num2;
 }
 
 function subtract(){
-    return num1 - num2
+    return +num1 - +num2;
 }
 
 function divide(){
-    return num1 / num2;
+    return +num1 / +num2;
 }
 
 function multiply(){
-    return num1 * num2;
+    return +num1 * +num2;
 }
 
 function operate(){
     if(operator == "+"){
-        return add(num1, num2);
+        display.value = add(num1, num2);
     } else if (operator == "-"){
-        return subtract(num1, num2);
+        display.value = subtract(num1, num2);
     } else if (operator == "/"){
-        return divide(num1, num2);
-    } else if (operator == '*'){
-        return multiply(num1, num2);
+        display.value = divide(num1, num2);
+    } else if (operator == 'x'){
+        display.value = multiply(num1, num2);
     }
 }
